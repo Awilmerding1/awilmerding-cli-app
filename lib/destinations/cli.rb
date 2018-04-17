8baedaf7f4f941bb1ed0fef4a699e5b5a1dbffae
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-17 17:03:22 -0400

    destination_more_info update

--- lib/destinations/cli.rb
+++ lib/destinations/cli.rb
@@ -13,6 +13,7 @@ class Destinations::CLI
     puts "Type 'exit' to exit the program."
   end
 
+
   def main_menu_select
     input = nil
     while input != "exit"
@@ -25,17 +26,17 @@ class Destinations::CLI
     if input.to_i == 2 || input.include?("cities")
       Destinations::TravelDestinations.scrape_top_lists("https://www.lonelyplanet.com/best-in-travel/cities")
       puts "If you would like to read about one of these destinations, please enter it's number on the list."
-      return
+      self.destination_more_info("https://www.lonelyplanet.com/best-in-travel/cities")
     end
     if input.to_i == 3 || input.include?("regions")
       Destinations::TravelDestinations.scrape_top_lists("https://www.lonelyplanet.com/best-in-travel/regions")
       puts "If you would like to read about one of these destinations, please enter it's number on the list."
-      return
+      self.destination_more_info("https://www.lonelyplanet.com/best-in-travel/regions")
     end
     if input.to_i == 4 || input.include?("value") || input.include?("best")
       Destinations::TravelDestinations.scrape_top_lists("https://www.lonelyplanet.com/best-in-travel/value")
       puts "If you would like to read about one of these destinations, please enter it's number on the list."
-      return
+      self.destination_more_info("https://www.lonelyplanet.com/best-in-travel/value")
     end
     end
   end
@@ -43,13 +44,10 @@ class Destinations::CLI
   def destination_more_info(input_url)
     more_input = nil
     while more_input != "exit"
-      more_input = gets.chomp.downcase
-      if more_input.to_i == 1
-        Destinations::TravelDestinations.scrape_for_description(input_url, more_input.to_i)
-      end
-      if more_input.to_i == 2
-        Destinations::TravelDestinations.scrape_for_description(input_url, more_input.to_i)
-      end
+    more_input = gets.chomp.downcase
+    if more_input.to_i.between?(1,10)
+    Destinations::TravelDestinations.scrape_for_description(input_url, more_input.to_i)
+  end
   end
 end
 
