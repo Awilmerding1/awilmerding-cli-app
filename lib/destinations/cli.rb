137cfe530671794cd167b21f5b81d7470aa0f6be
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-19 14:16:04 -0400

    exit and main menu

--- lib/destinations/cli.rb
+++ lib/destinations/cli.rb
@@ -7,19 +7,17 @@ class Destinations::CLI
 
   def intro
     Destinations::TravelDestinations.scrape_top_ten
-    puts "Welcome! Please select one of the lists above by number or keyword."
-    puts "Type 'exit' to exit the program."
+    puts "\nWelcome! Please select one of the lists above by number or keyword.\n\n"
+    puts "Type 'exit' to exit the program at anytime. Type 'main menu' to return to the list above at anytime."
   end
 
   def main_menu
     Destinations::TravelDestinations.scrape_top_ten
-    puts "Welcome! Please select another list."
+    puts "Please select a list."
     self.main_menu_select
   end
 
   def main_menu_select
-    input = nil
-    while input != "exit" && input != "main menu"
       input = gets.chomp.downcase
       link = nil
     if input.to_i == 1 || input.include?("countries")
@@ -38,10 +36,14 @@ class Destinations::CLI
       Destinations::TravelDestinations.scrape_top_value
       link = "https://www.lonelyplanet.com/best-in-travel/value"
     end
-    puts "If you would like to read about one of these destinations, please enter it's number on the list."
-    puts "To exit the program, enter 'exit'. To return to the main menu, enter 'main menu'."
-    self.destination_more_info(link)
+    if input.to_i == 'exit'
+      exit!
     end
+    if input.to_i == 'main menu'
+      self.main_menu
+    end
+    puts "\nIf you would like to read about one of these destinations, please enter it's number on the list.\n\n"
+    self.destination_more_info(link)
   end
 
   def destination_more_info(input_url)
